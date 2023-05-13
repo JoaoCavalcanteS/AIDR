d1bcf2f82e0dd953cc005a5aaa9ad4eb5a9b70ee
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2023-05-12 22:09:39 -0300

    CRUD
    Servidor de simulação de API, no qual retorna um JSON
    Uso do 'npm install -g json-server'

---
+++ angular/aula10/src/app/service/cliente.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../model/cliente';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+  //Se não colocar Private, ele não envia
+  //OBSERVABLE É OPCIONAL
+  constructor(private http:HttpClient) { }
+
+  //GET
+  listar(): Observable<Cliente[]>{
+    return this.http.get<Cliente[]>('http://localhost:3000/cliente');
+  }
+
+  //POST
+  inserir(cliente: Cliente):Observable<Cliente>{
+    return this.http.post<Cliente>('http://localhost:3000/cliente', cliente);//precisa do Id e os dados do cliente por inteiro
+  }
+  //PUT
+  atualizar(cliente: Cliente){
+    return this.http.put<Cliente>(`http://localhost:3000/cliente/${cliente.id}`, cliente);
+  }
+
+  //DELETE
+  //any nao se importa com o tipo
+  excluir(id: number):Observable<any>{
+    //delete nao precisa de tipo
+    return this.http.delete(`http://localhost:3000/cliente/${id}`);
+  }
+}
